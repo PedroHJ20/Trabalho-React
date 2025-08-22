37b4a0b3f3bd7accc11f390d322d7957461c7ef7
PedroHJ20 <Pedrohenriquetg13@gmail.com>
2025-08-22 23:51:25 +0000

    asdfghjkl

--- atvreact/components/Profile.js
+++ atvreact/components/Profile.js
@@ -3,7 +3,7 @@ import Image from "next/image"
 export function Profile() {
     return (
         <div>
-            <Image src="/perfil.jfif"alt="Imagem perfil" width={100} height={100}/ >
+            <Image src="/perfil.jpeg"alt="Imagem perfil" width={100} height={100}/ >
             <h4>Pedro Henrique Jerônimo</h4>
             <p>Sou estudante do 3 período de Sistemas para Internet</p>
             <p>Estou em fase de desenvolvimento </p>
